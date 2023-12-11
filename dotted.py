7745785b507d91d4b270999ad75cc60c54f57155
areebatahir <144821163+areebatahir@users.noreply.github.com>
2023-12-11 15:42:10 +0500

    Add files via upload

---
+++ dotted.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.linspace(0, 4*np.pi, 100)
+y1 = np.sin(x)
+y2 = np.sin(x + np.pi/4)
+y3 = np.sin(x + np.pi/2)
+y4 = np.sin(x + np.pi/4)
+y5 = np.sin(x + np.pi/2)
+plt.plot(x, y1, color = 'red', label = 'Sine Wave 1', linestyle = 'dashed')
+plt.plot(x, y2, color = 'blue', label = 'Sine Wave 2', linestyle = 'dotted')
+plt.plot(x, y3, color = 'black', label = 'Sine Wave 3', linestyle = 'dashed')
+plt.plot(x, y4, color = 'green', label = 'Sine Wave 4', linestyle = 'dashed')
+plt.plot(x, y5, color = 'pink', label = 'Sine Wave 5', linestyle = 'dashed')
+plt.title('Multiple Wave Lines')
+plt.xlabel('cos(x)')
+plt.ylabel('sin(x)')
+plt.legend()
+plt.show()
